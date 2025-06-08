334f5aec4755459097cb62e2bccbd9a2cf6379fa
I'm kotэ <egorsulga0@gmail.com>
2025-06-08 15:58:56 +0300

    comit

--- IPcalc/main.cpp
+++ IPcalc/main.cpp
@@ -6,12 +6,11 @@
 #include <cstdio>
 #include "resource.h"
 
+#define BUFFER_SIZE 1024
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-//int MaskToPrefix(DWORD dwMask);
-//DWORD PrefixToMask(int prefix);
-
-//bool checkBoxPrefix = false;
+VOID PrintInfo(HWND hwnd);
+CHAR* IPtoString(DWORD dwIpAddres, CHAR sz_IpAddress[]);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -27,7 +26,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 
 		HWND hPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
-		SendMessage(hPrefix, UDM_SETRANGE, 0, MAKELPARAM(32, 0));
+		SendMessage(hPrefix, UDM_SETRANGE, 0, MAKELPARAM(30, 1));
 		SetFocus(GetDlgItem(hwnd, IDC_IPADDRESS_IP));
 
 		AllocConsole();
@@ -46,19 +45,31 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case IDC_IPADDRESS_IP:
 		{
 			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIpAdddress);
-			if (FIRST_IPADDRESS(dwIpAdddress) < 128) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFF000000);
-			else if (FIRST_IPADDRESS(dwIpAdddress) < 192) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFF0000);
-			else if (FIRST_IPADDRESS(dwIpAdddress) < 224) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFFFF00);
+			if (FIRST_IPADDRESS(dwIpAdddress) < 128) SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"8");
+			else if (FIRST_IPADDRESS(dwIpAdddress) < 192) SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"16");
+			else if (FIRST_IPADDRESS(dwIpAdddress) < 224) SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"24");
 		}break;
 
-		case IDC_IPADDRESS_MASK:
+		case IDC_SPIN_PREFIX:
 		{
-			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIpMask);
-			DWORD dwIPprefix = 0;
-			for (DWORD iMask = dwIpMask; iMask & 0x80000000; dwIPprefix++) iMask <<= 1;
-			CHAR sz_prefix[3];
-			sprintf(sz_prefix, "%i", dwIPprefix);
-			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)sz_prefix);
+			std::cout << "WM_NOTIFE:EDC_SPIN_PREFIX" << std::endl;
+
+			DWORD dwPrefix = ((NMUPDOWN*)lParam)->iPos;
+			INT iDelta = ((NMUPDOWN*)lParam)->iDelta;
+			dwPrefix += iDelta;
+			std::cout << dwPrefix << std::endl;
+			DWORD dwIPMask = ~(0xFFFFFFFF >> dwPrefix);
+			SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPMask);
+		} break;
+
+		case IDC_EDIT_PREFIX:
+		{
+			CHAR sz_prefix[3] = {};
+			SendMessage(hEditPrefix, WM_GETTEXT, 3, (LPARAM)sz_prefix);
+			DWORD dwPrefix = atoi(sz_prefix);
+			DWORD dwIPMask = ~(0xFFFFFFFF >> dwPrefix);
+			SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPMask);
+			PrintInfo(hwnd);
 		} break;
 
 
@@ -76,22 +87,22 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 		HWND hIPmask = GetDlgItem(hwnd, IDC_IPADDRESS_MASK);
-
-		//switch (((NMHDR*)lParam)->idFrom)
-		switch (LOWORD(wParam))
+		DWORD dwIpMask = 0;
+		
+		switch (((NMHDR*)lParam)->idFrom)
+		//switch (LOWORD(wParam))
 		{
-		case IDC_SPIN_PREFIX:
+		case IDC_IPADDRESS_MASK:
 		{
-			std::cout << "WM_NOTIFE:EDC_SPIN_PREFIX" << std::endl;
-			
-			DWORD dwPrefix = ((NMUPDOWN*)lParam)->iPos;
-			INT iDelta = ((NMUPDOWN*)lParam)->iDelta;
-			dwPrefix += iDelta;
-			std::cout << dwPrefix << std::endl;
-			DWORD dwIPMask = ~(0xFFFFFFFF >> dwPrefix);
-			SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPMask);
+			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIpMask);
+			DWORD dwIPprefix = 0;
+			for (DWORD iMask = dwIpMask; iMask & 0x80000000; dwIPprefix++) iMask <<= 1;
+			CHAR sz_prefix[3];
+			sprintf(sz_prefix, "%i", dwIPprefix);
+			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)sz_prefix);
 		} break;
-		}
+
+		} //switch
 	} break;
 
 	case WM_CLOSE:
@@ -101,25 +112,46 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return FALSE;
 }
 
-int MaskToPrefix(DWORD dwMask) 
+VOID PrintInfo(HWND hwnd)
 {
-	int prefix = 0;
-	DWORD mask = dwMask;
+	HWND hIpAddress = GetDlgItem(hwnd, IDC_IPADDRESS_IP);
+	HWND hIpMask = GetDlgItem(hwnd, IDC_IPADDRESS_MASK);
+	HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+	DWORD dwIpAddress = 0;
+	DWORD dwIpMask = 0;
+	
+	SendMessage(hIpAddress, IPM_GETADDRESS, 0, (LPARAM)&dwIpAddress);
+	SendMessage(hIpMask, IPM_GETADDRESS, 0, (LPARAM)&dwIpMask);
+	DWORD dwNetworkAddress = dwIpAddress & dwIpMask;
+	DWORD dwBroadcastAddress = dwIpAddress | ~dwIpMask;
+	DWORD dwNumberOfAddresses = ~dwIpMask + 1;
+	DWORD dwNumberOfHost = ~dwIpMask - 1;
 
-	while (mask & 0x80000000) 
-	{
-		prefix++;
-		mask <<= 1;
-	}
-
-	// если после единиц идут нули, это валидная маска
-	if (mask != 0) return -1; // некорректная маска (ошибка)
-
-	return prefix;
+	CHAR sz_NetworkAddress[16] = {};
+	CHAR sz_BroadcastAddress[16] = {};
+	CHAR sz_info[BUFFER_SIZE] = {};
+	sprintf
+	(
+		sz_info,
+		"Info:\nАдрес сети:\t\t\t%s;\nШироковещательный адрес:\t%s;\nКоличество Ip-адресов:\t%i;\nКоличество узлов:\t\t%i",
+		IPtoString(dwNetworkAddress, sz_NetworkAddress),
+		IPtoString(dwBroadcastAddress, sz_BroadcastAddress),
+		dwNumberOfAddresses,
+		dwNumberOfHost
+	);
+	SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)sz_info);
 }
 
-DWORD PrefixToMask(int prefix) 
+CHAR* IPtoString(DWORD dwIpAddres, CHAR sz_IpAddress[])
 {
-	if (prefix < 0 || prefix > 32) return 0xFFFFFFFF; // некоректный префикс возвращаем маску по умолчанию
-	return (prefix == 0) ? 0 : (0xFFFFFFFF << (32 - prefix)); // создаём маску с N колво едениц слева
+	sprintf
+	(
+		sz_IpAddress, 
+		"%i.%i.%i.%i",
+		FIRST_IPADDRESS(dwIpAddres),
+		SECOND_IPADDRESS(dwIpAddres),
+		THIRD_IPADDRESS(dwIpAddres),
+		FOURTH_IPADDRESS(dwIpAddres)
+	);
+	return sz_IpAddress;
 }
